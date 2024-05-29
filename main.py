e26aa5e700579aadd63f586ab6e376cf8d8a7567
Alex Muirhead <alexandermuirhead@hotmail.com>
2024-05-29 14:58:53 +1000

    WIP add args for config and output

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from dataclasses import dataclass
 from pathlib import PurePath
 
@@ -15,7 +16,7 @@ import papersize
 from starcover import observer
 from starcover.schema import Config
 
-from util import stereographicProjection, cylindricalProjection
+from util import stereographicProjection, cylindricalProjection, shiftedCylindricalProjection
 
 
 RESOLUTION = 1000
@@ -35,7 +36,7 @@ class Page:
 
     @classmethod
     def fromString(cls, string: str) -> 'Page':
-        width, height = papersize.parse_papersize(string, unit = cls.unit)
+        width, height = papersize.parse_papersize(string, unit=cls.unit)
         return Page(width=float(width), height=float(height))
 
     @property
@@ -64,112 +65,143 @@ class Display:
         return (-self.width/2, -self.height/2)
 
 
-with open('config.toml') as file:
-    config = Config(**toml.load(file).unwrap())
+def main(config_file: str, output_file: str | None):
 
-page = Page.fromString(config.output.size)
-display = Display.fromPage(page, resolution=5)
+    with open(config_file) as file:
+        config = Config(**toml.load(file).unwrap())
 
-# The Hipparcos mission provides our star catalog.
+    page = Page.fromString(config.output.size)
+    display = Display.fromPage(page, resolution=5)
 
-with load.open(hipparcos.URL) as file:
-    stars = hipparcos.load_dataframe(file)
-    # Do some data cleaning. Sort and fill, so the hip ID corresponds to row index
-    stars = stars.reindex(index=pd.RangeIndex(0, stars.index.max()+1), fill_value=np.nan)
+    # The Hipparcos mission provides our star catalog.
 
-alt, az, _ = observer.fromConfig(config.observer) \
-    .observe(Star.from_dataframe(stars)) \
-    .apparent() \
-    .altaz()
+    with load.open(hipparcos.URL) as file:
+        stars = hipparcos.load_dataframe(file)
+        # Do some data cleaning. Sort and fill, so the hip ID corresponds to row index
+        stars = stars.reindex(index=pd.RangeIndex(0, stars.index.max()+1), fill_value=np.nan)
 
-projection = cylindricalProjection
-# projection = stereographicProjection
-star_centers = np.stack(projection(alt.radians, az.radians), axis=1)
-star_centers = np.round(star_centers * display.size / 2, decimals=2)
+    alt, az, _ = observer.fromConfig(config.observer) \
+        .observe(Star.from_dataframe(stars)) \
+        .apparent() \
+        .altaz()
+
+    projection = shiftedCylindricalProjection
+    # projection = cylindricalProjection
+    # projection = stereographicProjection
+    star_centers = np.stack(projection(alt.radians, az.radians), axis=1)
+    star_centers = np.round(star_centers * display.size / 2, decimals=2)
+
+    def brightness(magnitude):
+        return 20 * 100**(-0.02*magnitude)
+
+    star_markers  = brightness(stars['magnitude'].values)
+    star_markers -= brightness(config.output.max_magnitude)  # Normalise
+    star_markers  = np.round(star_markers, decimals=1)
+    bright, = np.where(np.logical_and(
+        # Ensure stars are bright enough
+        star_markers > 1,
+        # Ensure stars are within view-field
+        abs(star_centers[:, 0]) - 2*star_markers <= display.width / 2,
+        abs(star_centers[:, 1]) - 2*star_markers <= display.height / 2,
+    ))
+
+    # The constellation outlines come from Stellarium. We make a list of the stars
+    # at which each edge stars, and the star at which each edge ends.
+
+    url = ("https://raw.githubusercontent.com/Stellarium/stellarium/"
+           "master/skycultures/modern_st/constellationship.fab")
+
+    with load.open(url) as file:
+        constellations = stellarium.parse_constellations(file)
+
+    # Time to build the map!
+
+    output_name = output_file or config.output.name
+    dwg = svg.Drawing(
+        filename=str(PurePath(output_name).with_suffix(".svg")),
+        size=page.size
+    )
+    dwg.viewbox(
+        minx=-display.width//2, miny=-display.height//2,
+        width=display.width, height=display.height
+    )
+
+    clip_path = dwg.defs.add(dwg.clipPath(id="clipCanvas"))
+    clip_path.add(Rect(insert=display.bottomLeft, size=display.size))
+
+    star_group = dwg.g(
+        clip_path=clip_path.get_funciri(),
+        **config.style.stars.flat_dump()
+    )
+
+    constellation_group = dwg.g(
+        clip_path=clip_path.get_funciri(),
+        **config.style.constellations.flat_dump()
+    )
+
+    for center, marker in zip(star_centers[bright], star_markers[bright]):
+        # Move checking for the center to a filter here
+        star_group.add(Circle(center=center, r=marker))
+
+        # Clips over right edge
+        if center[0] + 2*marker > display.width / 2:
+            fakeCenter = center - [display.width, 0]
+            star_group.add(Circle(center=fakeCenter, r=marker))
+
+        # Clips over left edge
+        elif center[0] - 2*marker < -display.width / 2:
+            fakeCenter = center + [display.width, 0]
+            star_group.add(Circle(center=fakeCenter, r=marker))
+
+        if center[1] + 2*marker > display.height / 2:
+            fakeCenter = [center[0] % display.width - display.width / 2, display.height - center[1]]
+            star_group.add(Circle(center=fakeCenter, r=marker))
+
+        elif center[1] - 2*marker < -display.height / 2:
+            fakeCenter = [center[0] % display.width - display.width / 2, -display.height - center[1]]
+            star_group.add(Circle(center=fakeCenter, r=marker))
+
+    for name, edges in constellations:
+        for (startID, endID) in edges:
+            start, end = star_centers[[startID, endID]]
+
+            if any(np.isnan(start)):
+                print(f"Skipping ill-defined star {startID} in constellation {name}")
+                continue
+            elif any(np.isnan(end)):
+                print(f"Skipping ill-defined star {endID} in constellation {name}")
+                continue
+
+            # Check if we've crossed the "split" on the sky at az = 2pi
+            # if az.radians[startID] - az.radians[endID] > np.pi:
+            if start[0] - end[0] > display.width / 2:
+                # Create two new fake stars, and duplicate the lines
+                fakeStart = start - [display.width, 0]
+                fakeEnd = end + [display.width, 0]
+
+                constellation_group.add(Line(start=fakeStart, end=end))
+                constellation_group.add(Line(start=start, end=fakeEnd))
+
+            # elif az.radians[endID] - az.radians[startID] > np.pi:
+            elif end[0] - start[0] > display.width / 2:
+                # Create two new fake stars, and duplicate the lines
+                fakeStart = start + [display.width, 0]
+                fakeEnd = end - [display.width, 0]
+
+                constellation_group.add(Line(start=fakeStart, end=end))
+                constellation_group.add(Line(start=start, end=fakeEnd))
+
+            else:
+                constellation_group.add(Line(start=start, end=end))
+
+    dwg.add(star_group)
+    dwg.add(constellation_group)
+    dwg.save(pretty=True, indent=4)
 
 
-def brightness(magnitude):
-    return 20 * 100**(-0.02*magnitude)
-
-
-star_markers  = brightness(stars['magnitude'].values)
-star_markers -= brightness(config.output.max_magnitude)  # Normalise
-star_markers  = np.round(star_markers, decimals=1)
-bright, = np.where(np.logical_and(
-    # Ensure stars are bright enough
-    star_markers > 1,
-    # Ensure stars are within view-field
-    np.all(abs(star_centers) <= 1000, axis=1),
-))
-
-# The constellation outlines come from Stellarium. We make a list of the stars
-# at which each edge stars, and the star at which each edge ends.
-
-url = ("https://raw.githubusercontent.com/Stellarium/stellarium/"
-       "master/skycultures/modern_st/constellationship.fab")
-
-with load.open(url) as file:
-    constellations = stellarium.parse_constellations(file)
-
-# Time to build the map!
-
-dwg = svg.Drawing(
-    filename=str(PurePath(config.output.name).with_suffix(".svg")),
-    size=page.size
-)
-dwg.viewbox(
-    minx=-display.width//2, miny=-display.height//2, 
-    width=display.width, height=display.height
-)
-
-clip_path = dwg.defs.add(dwg.clipPath(id="clipCanvas"))
-clip_path.add(Rect(insert=display.bottomLeft, size=display.size))
-
-star_group = dwg.g(
-    clip_path=clip_path.get_funciri(),
-    **config.style.stars.flat_dump()
-)
-
-constellation_group = dwg.g(
-    clip_path=clip_path.get_funciri(),
-    **config.style.constellations.flat_dump()
-)
-
-for center, marker in zip(star_centers[bright], star_markers[bright]):
-    # Move checking for the center to a filter here
-    star_group.add(Circle(center=center, r=marker))
-
-for name, edges in constellations:
-    for (startID, endID) in edges:
-        start, end = star_centers[[startID, endID]]
-
-        if any(np.isnan(start)):
-            print(f"Skipping ill-defined star {startID} in constellation {name}")
-            continue
-        elif any(np.isnan(end)):
-            print(f"Skipping ill-defined star {endID} in constellation {name}")
-            continue
-
-        # Check if we've crossed the "split" on the sky at az = 2pi
-        if az.radians[startID] - az.radians[endID] > np.pi:
-            # Create two new fake stars, and duplicate the lines
-            fakeStart = start - [display.width, 0]
-            fakeEnd = end + [display.width, 0]
-
-            constellation_group.add(Line(start=fakeStart, end=end))
-            constellation_group.add(Line(start=start, end=fakeEnd))
-
-        elif az.radians[endID] - az.radians[startID] > np.pi:
-            # Create two new fake stars, and duplicate the lines
-            fakeStart = start + [display.width, 0]
-            fakeEnd = end - [display.width, 0]
-
-            constellation_group.add(Line(start=fakeStart, end=end))
-            constellation_group.add(Line(start=start, end=fakeEnd))
-
-        else:
-            constellation_group.add(Line(start=start, end=end))
-
-dwg.add(star_group)
-dwg.add(constellation_group)
-dwg.save(pretty=True, indent=4)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Star Cover")
+    parser.add_argument('-c', "--config", default="config.toml")
+    parser.add_argument('-o', "--output")
+    args = parser.parse_args()
+    main(config_file=args.config, output_file=args.output)
